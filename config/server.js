fce24441cc887dbfc846e915dfc47d9d6768b85e
Bart Sakowski <bartsakowski@Barts-Air.attlocal.net>
2021-07-20 21:15:34 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '17e76c9c2926ff02b8157957e31315ea'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6c3140a6-1524-42ee-9a7c-6b5c0e661c02'
+};
